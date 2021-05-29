2079821226f31697748ec4ceeb9065e7b51dbf64
Verge <981340404@qq.com>
2021-05-29 16:07:18 +0800

    fix<detail>: fix some bugs

--- miniprogram/pages/detail/detail.js
+++ miniprogram/pages/detail/detail.js
@@ -19,23 +19,23 @@ Page({
   //点开某楼层
   onOpen(event) {
     let that = this
-    let floor = event.detail
-    let point_id = that.data.point_id
-    let description = []
+    let floor = event.currentTarget.dataset.floor
+    let point_id = this.data.point_id
+    console.log("floor_num:" + floor);
+    console.log("point_id:" + point_id);
+    
     wx.cloud.callFunction({
       name: 'units-point',
       data: {
         point_id,
         floor
-      },
-      success: res => {
-        // console.log('onOpen-res:',res.result);
-        description = res.result
-        that.setData({
-          description
-        })
-        console.log('description:', that.data.description);
       }
+    }).then(res => {
+      let units_floor = res.result
+      this.setData({
+        units_floor
+      })
+      console.log(res);
     })
   },
 
@@ -62,19 +62,21 @@ Page({
       point_id
     }),
     wx.cloud.callFunction({
-      name: 'units-point',
+      name: 'point',
       data: {
         point_id
-      },
-      success: res => {
-        console.log('units-point-res:',res.result);
-        let floor_detail = []
-        floor_detail = res.result
-        that.setData({
-          floor_detail
-        })
-        console.log('floor:',floor_detail);
       }
+    }).then(res => {
+      console.log('units-point-res:',res.result);
+      let floor_num = res.result.floors
+      let floor_array = [];
+      for (let index = 1; index <= floor_num; index++) {
+        floor_array.push(index)
+      }
+      that.setData({
+        floor_array
+      })
+      console.log('floor_num:'+floor_array);
     })
   },
 
